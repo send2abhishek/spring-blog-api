4d265f3de6a8901a1f45754844ea3066ca020aa0
Abhishek Kumar <abhishek160.kumar@ril.com>
2022-04-28 22:19:28 +0530

    feat: modify interceptor

--- src/main/java/com/spring/blog/blogrestapi/configuration/LogConfig.java
+++ src/main/java/com/spring/blog/blogrestapi/configuration/LogConfig.java
@@ -1,12 +1,10 @@
 package com.spring.blog.blogrestapi.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class LogConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
