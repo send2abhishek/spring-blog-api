4527ebd792d051cead5bcd70b794dd4b15da1d2d
Abhishek Kumar <abhishek160.kumar@ril.com>
2022-05-01 09:01:59 +0530

    feat: Add static file serving

--- src/main/java/com/spring/blog/blogrestapi/configuration/LogConfig.java
+++ src/main/java/com/spring/blog/blogrestapi/configuration/LogConfig.java
@@ -2,12 +2,25 @@ package com.spring.blog.blogrestapi.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class LogConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ApiLogger()).addPathPatterns("/*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
